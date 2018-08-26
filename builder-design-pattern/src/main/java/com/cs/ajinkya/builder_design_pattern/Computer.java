f9f188fb0bc49f22536f7bea973e9c5629cfd09e
AJ <ajinkya.kulkarni77@gmail.com>
2018-08-25 21:53:53 -0700

    Created builder class and private Computer constructor

--- builder-design-pattern/src/main/java/com/cs/ajinkya/builder_design_pattern/Computer.java
+++ builder-design-pattern/src/main/java/com/cs/ajinkya/builder_design_pattern/Computer.java
@@ -11,6 +11,13 @@ public class Computer {
 	private boolean isWIFIEnabled;
 	private boolean isBluetoothEnabled;
 	
+	private Computer(Builder builder) {
+		this.RAM = builder.RAM;
+		this.CPU = builder.CPU;
+		this.HDD = builder.HDD;
+		this.isBluetoothEnabled = builder.isBluetoothEnabled;
+		this.isWIFIEnabled = builder.isWIFIEnabled;
+	}
 	public String getRAM() {
 		return RAM;
 	}
@@ -25,6 +32,52 @@ public class Computer {
 	}
 	public boolean isBluetoothEnabled() {
 		return isBluetoothEnabled;
+	}
+	
+	public static class Builder {
+		
+		// required parameters
+		private String RAM;
+		private String HDD;
+		private String CPU;
+
+		// optional parameters
+		private boolean isWIFIEnabled;
+		private boolean isBluetoothEnabled;
+		
+		public Builder(String RAM, String HDD, String CPU) {
+			this.RAM = RAM;
+			this.HDD = HDD;
+			this.CPU = CPU;
+		}
+
+		public void setRAM(String rAM) {
+			RAM = rAM;
+		}
+
+		public void setHDD(String hDD) {
+			HDD = hDD;
+		}
+
+		public void setCPU(String cPU) {
+			CPU = cPU;
+		}
+
+		public Builder setWIFIEnabled(boolean isWIFIEnabled) {
+			this.isWIFIEnabled = isWIFIEnabled;
+			return this;
+		}
+
+		public Builder setBluetoothEnabled(boolean isBluetoothEnabled) {
+			this.isBluetoothEnabled = isBluetoothEnabled;
+			return this;
+		}
+		
+		public Computer build() {
+			return new Computer(this);
+			
+		}
+		
 	}
 
 }
