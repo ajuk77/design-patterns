9257aeb63225b73391e46271609e25f5af8cb2a6
AJ <ajinkya.kulkarni77@gmail.com>
2018-08-25 20:43:52 -0700

    Adding project skeleton

---
+++ builder-design-pattern/src/main/java/com/cs/ajinkya/builder_design_pattern/App.java
@@ -0,0 +1,13 @@
+package com.cs.ajinkya.builder_design_pattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
