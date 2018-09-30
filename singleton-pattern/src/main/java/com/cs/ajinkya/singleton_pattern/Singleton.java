fe3cb713adf30a1aa52dc413ec9a09a5adf80280
AJ <ajinkya.kulkarni77@gmail.com>
2018-09-30 13:37:09 -0700

    Added double check locking in singleton multi-threading scenario

--- singleton-pattern/src/main/java/com/cs/ajinkya/singleton_pattern/Singleton.java
+++ singleton-pattern/src/main/java/com/cs/ajinkya/singleton_pattern/Singleton.java
@@ -3,32 +3,40 @@ package com.cs.ajinkya.singleton_pattern;
 import java.io.ObjectStreamException;
 import java.io.Serializable;
 
-public class Singleton implements Serializable, Cloneable{
-	
-	private static Singleton soleInstance = null;
-	
+public class Singleton implements Serializable, Cloneable {
+	// using volatile keyword in multi threading env. Avoid creation of half baked
+	// objects
+	private static volatile Singleton soleInstance = null;
+
 	private Singleton() {
-		//In case of reflection
-		if(null != soleInstance) {
+		// In case of reflection
+		if (null != soleInstance) {
 			throw new RuntimeException("Cannot create, please use getInstance()");
 		}
-		//proceed with creation
+		// proceed with creation
 		System.out.println("Creating an instance");
 	}
-	
+
+	// In case of multithreading
 	public static Singleton getInstance() {
-		if(null == soleInstance) {
-			soleInstance = new Singleton(); // lazy initialization
+		if (null == soleInstance) { // check 1
+			synchronized (Singleton.class) {
+				if (soleInstance == null) { // check 2: double check locking
+					soleInstance = new Singleton(); // lazy initialization
+				}
+			}
+
 		}
 		return soleInstance;
 	}
-	
+
 	@Override
 	protected Object clone() throws CloneNotSupportedException {
+		// In case of clonning
 		return soleInstance;
 	}
-	
-	//In case of serialization
+
+	// In case of serialization
 	private Object readResolve() throws ObjectStreamException {
 		System.out.println("Read resolved");
 		return soleInstance;
