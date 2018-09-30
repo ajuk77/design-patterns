ddc815ddfe91bb06ce15cdc0d6266e19743b5a39
AJ <ajinkya.kulkarni77@gmail.com>
2018-09-30 10:24:27 -0700

    Using reflection to break the singleton pattern

--- singleton-pattern/src/main/java/com/cs/ajinkya/singleton_pattern/App.java
+++ singleton-pattern/src/main/java/com/cs/ajinkya/singleton_pattern/App.java
@@ -1,18 +1,27 @@
 package com.cs.ajinkya.singleton_pattern;
 
+import java.lang.reflect.Constructor;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
         Singleton s1 = Singleton.getInstance();
         Singleton s2 = Singleton.getInstance();
         
         print("s1", s1);
         print("s2", s2);
+        
+        //Reflection
+        Class clazz = Class.forName("com.cs.ajinkya.singleton_pattern.Singleton");
+        Constructor<Singleton> ctor = clazz.getDeclaredConstructor(null);
+        ctor.setAccessible(true);
+        Singleton s3 = ctor.newInstance(null);
+        print("s3", s3);
     }
 
 	private static void print(String name, Singleton object) {
